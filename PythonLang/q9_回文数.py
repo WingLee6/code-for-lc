eed5843188393f09fba5b9fc786632185b2196b6
Lee <luke_lee_98@163.com>
2022-06-26 16:41:56 +0800

    python 1. q9 回文数

--- PythonLang/q9_回文数.py
+++ PythonLang/q9_回文数.py
@@ -45,7 +45,27 @@ class Solution:
 
 
 if __name__ == '__main__':
-    num = 10
+    num = 123456787654321
     solution = Solution()
     result = solution.isPalindrome(x=num)
     print(result)
+
+"""
+示例 1：
+输入：x = 121
+输出：true
+
+示例 2：
+输入：x = -121
+输出：false
+解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+
+示例 3：
+输入：x = 10
+输出：false
+解释：从右向左读, 为 01 。因此它不是一个回文数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/palindrome-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
