c2d502800b2b60042a37070e61139a74fd75e93b
Luke Lee <17501681070@163.com>
2022-07-22 01:01:08 +0800

    py q118

--- PythonLang/118.杨辉三角.py
+++ PythonLang/118.杨辉三角.py
@@ -11,6 +11,23 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        return []
+        layer = []
+        for i in range(numRows):
+            layer.append([1]*(i+1))
+            for j in range(i-1):
+                layer[i][j+1] = layer[i-1][j] + layer[i-1][j+1]
+
+        return layer
 # @lc code=end
 
+
+"""
+input 5
+output [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+input 1
+output [[1]]
+
+input 5
+output [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+"""
