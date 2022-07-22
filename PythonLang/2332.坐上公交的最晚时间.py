53d918a41f62b5b4868abb459206bc68802c4d72
Luke Lee <17501681070@163.com>
2022-07-22 10:44:01 +0800

    q2332

---
+++ PythonLang/2332.坐上公交的最晚时间.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode.cn id=2332 lang=python3
+#
+# [2332] 坐上公交的最晚时间
+#
+
+# @lc code=start
+class Solution:
+    def latestTimeCatchTheBus(self, buses: List[int], passengers: List[int], capacity: int) -> int:
+
+
+        return 18
+# @lc code=end
+
